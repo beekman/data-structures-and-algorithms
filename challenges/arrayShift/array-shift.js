4ef4b6ae72a36585a0bb207f0415365ca8c130ae
Ben Beekman <bbeekman@gmail.com>
2019-12-03 17:19:54 -0800

    completed stretch goal and regular goal

--- challenges/arrayShift/array-shift.js
+++ challenges/arrayShift/array-shift.js
@@ -1,3 +1,30 @@
-export function insertShiftArray(arr) {
-    
-}
+const arrayShift = (array, value) => {
+  const newArray = [];
+  const midIndex = Math.ceil((array.length) / 2);
+  for(let i = 0;i < array.length + 1;i++) {
+    if(i < midIndex) {
+      newArray[i] = array[i];
+    }
+    if(i === midIndex) {
+      newArray[i] = value;
+    }
+    if(i > midIndex) {
+      newArray[i] = array[i - 1];
+    }
+  }
+  return newArray;
+};
+const arrayRemoveMiddle = (array) => {
+  const newArray = [];
+  const midIndex = Math.floor((array.length) / 2);
+  for(let i = 0;i < array.length - 1;i++) {
+    if(i < midIndex) {
+      newArray[i] = array[i];
+    }
+    if(i >= midIndex) {
+      newArray[i] = array[i + 1];
+    }
+  }
+  return newArray;
+};
+module.exports = { arrayShift, arrayRemoveMiddle };
