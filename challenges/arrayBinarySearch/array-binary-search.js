08bb87d422d01b1535818633784a59ffdef4abf0
Ben Beekman <bbeekman@gmail.com>
2019-12-09 13:02:19 -0800

    working function and full coverage tests

--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,15 +1,23 @@
 
 const BinarySearch = (arr, key) => {
-  let mid = (Math.floor((arr.length - 1) / 2));
-  if(arr[mid] === key) return mid;
-  else if(arr[mid] > key) {
-    mid = Math.floor(mid / 2);
+  let max = arr.length - 1;
+  let min = 0;
+  let mid = (Math.floor((max + min) / 2));
+  while((arr[mid] != key) && min != max) {
+
+    if(arr[mid] < key) {
+      min = mid + 1;
+      mid = (Math.floor((max + min) / 2));
+    }
+
+    if(arr[mid] > key) {
+      max = mid - 1;
+      mid = (Math.floor((max + min) / 2));
+    }
   }
 
-  if(arr[mid] === key) return arr[mid];
-  return arr[mid];
-
-
+  if(arr[mid] === key) return mid;
+  else return -1;
 };
 
 module.exports = { BinarySearch };
