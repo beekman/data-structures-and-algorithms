fad5344794f290cb602e55b2e23803f623ba1d13
Ben Beekman <bbeekman@gmail.com>
2019-12-18 20:23:00 -0800

    untested classes written

--- challenges/fifoAnimalShelter/fifo-animal-shelter.js
+++ challenges/fifoAnimalShelter/fifo-animal-shelter.js
@@ -1,88 +1,47 @@
+const { Stack } = require('../queueWithStacks/queue-with-stacks');
+
 class AnimalShelter {
   constructor() {
-    
+    this.dogQueue = new Stack();
+    this.catQueue = new Stack();
   }
 
-  enqueue(animal) {
-    if(animal.type === 'Dog') {
-      dogQueue.front = animal;
-    } else if(animal.type === 'Cat') {
-      if(catQueue.front) {
-        catQueue.next = catQueue.front;
-      }
-      catQueue.front = animal;
-      this.frontCat = animal;
+  enqueue(Animal) {
+    if(Animal.type === 'Dog') {
+      this.dogQueue.push(Animal);
+    } else if(Animal.type === 'Cat') {
+      this.catQueue.push(Animal);
+    }
+  }
+
+  dequeue(Animal) {
+    if(Animal.type === 'Dog') {
+      this.dogQueue.pop(Animal);
+    } else if(Animal.type === 'Cat') {
+      this.catQueue.pop(Animal);
     }
   }
 }
 
-class Node {
-  constructor(value, next = null) {
-    this.value = value;
-    this.next = next;
+class Animal {
+  constructor(name, type) {
+    this.name = name;
+    this.type = type;
   }
 }
 
-class Stack {
-  constructor() {
-    this.top = null;
-  }
-
-  push(value) {
-    const node = new Node(value, this.top);
-    this.top = node;
-  }
-
-  pop() {
-    if(!this.top) return null;
-
-    const top = this.top;
-    this.top = top.next;
-    return top;
-  }
-
-  peek() {
-    if(!this.top) return null;
-    return this.top.value;
-  }
-
-  isEmpty() {
-    return !this.top;
+class Cat extends Animal {
+  constructor(name) {
+    super(name);
+    this.type = 'Cat';
   }
 }
 
-class Queue {
-  constructor() {
-    this.front = null;
-    this.back = null;
-  }
-
-  enqueue(value) {
-    const node = new Node(value, null);
-    if(this.isEmpty()) {
-      this.front = node;
-    } else {
-      this.back.next = node;
-    }
-    this.back = node;
-  }
-
-  dequeue() {
-    if(this.isEmpty()) return null;
-
-    const front = this.front;
-    this.front = this.front.next;
-    if(this.isEmpty()) this.back = null;
-
-    return front;
-  }
-
-  peek() {
-    if(this.isEmpty()) return null;
-    return this.front.value;
-  }
-
-  isEmpty() {
-    return !this.front;
+class Dog extends Animal {
+  constructor(name) {
+    super(name);
+    this.type = 'Dog';
   }
 }
+
+module.exports = { Animal, Cat, Dog, AnimalShelter };
