9ce29f83ccb6a7de12ee98cc573372a60c92b0b4
Ben Beekman <bbeekman@gmail.com>
2020-01-29 13:43:54 -0800

    numerous tree fixes

--- challenges/tree/tree.js
+++ challenges/tree/tree.js
@@ -4,44 +4,52 @@ class Node {
     this.value = val;
     this.left = leftChild;
     this.right = rightChild;
-    this.show = show;
   }
 }
-
-const show = () => {
-  return this.value;
-};
 class Tree {
-  constructor() {
-    this.root = null;
-    this.inOrder = inOrder;
-    this.preOrder = preOrder;
-    this.postOrder = postOrder;
+
+  constructor(root) {
+    this.root = root;
+  }
+
+  inOrder() {
+    let results = [];
+
+    let _walk = (node) => {
+      if(node.left) { _walk(node.left); }
+      results.push(node.value);
+      if(node.right) { _walk(node.right); }
+    };
+    _walk(this.root);
+
+    return results.toString();
+  }
+
+  preOrder() {
+    let results = [];
+
+    let _walk = (node) => {
+      results.push(node.value);
+      if(node.left) { _walk(node.left); }
+      if(node.right) { _walk(node.right); }
+    };
+    _walk(this.root);
+
+    return results.toString();
+  }
+
+  postOrder() {
+    let results = [];
+
+    let _walk = (node) => {
+      if(node.left) { _walk(node.left); }
+      if(node.right) { _walk(node.right); }
+      results.push(node.value);
+    };
+    _walk(this.root);
+
+    return results.toString();
   }
 }
 
-const inOrder = (Node) => {
-  if(!(Node == null)) {
-    inOrder(Node.left);
-    console.log(Node.show() + ' ');
-    inOrder(Node.right);
-  }
-};
-
-const preOrder = (Node) => {
-  if(!(Node == null)) {
-    console.log(Node.show() + ' ');
-    preOrder(Node.left);
-    preOrder(Node.right);
-  }
-};
-
-const postOrder = (Node) => {
-  if(!(Node == null)) {
-    postOrder(Node.left);
-    postOrder(Node.right);
-    console.log(Node.show() + ' ');
-  }
-};
-
-module.exports = { Node, Tree, inOrder, postOrder, preOrder };
+module.exports = { Node, Tree };
