da5e3535102a10b771cba578d2166983110e91b4
Ben Beekman <bbeekman@gmail.com>
2021-05-10 23:53:24 -0700

    commented countPrimes

--- challenges/may-2021-leetcode-challenge/countPrimes.js
+++ challenges/may-2021-leetcode-challenge/countPrimes.js
@@ -1,4 +1,5 @@
 let countPrimes = (n) => {
+   // pre-populate an unPrimes array with n values of false
    let unPrimes = new Array(n);
    for(let i = 0; i < n; i++) {
       unPrimes[i] = false;
@@ -7,6 +8,7 @@ let countPrimes = (n) => {
    for(let i = 2; i < n; i++) {
       if(unPrimes[i] === false) {
          result++;
+         //mark all multiples of n up to the end of the array as unPrime
          for(let j = 2; i * j < n; j++) {
             unPrimes[i * j] = true;
          }
