c572059a6931feab7980c9bca901579e1478d12f
Ben Beekman <bbeekman@gmail.com>
2021-05-27 23:52:42 -0700

    solution to daily leetcode challenge

---
+++ challenges/may-2021-leetcode-challenge/maxProduct.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string[]} words
+ * @return {number}
+ *
+ * Maximum Product of Word Lengths
+Given a string array words, return the maximum value of length(word[i]) * length(word[j]) where the two words do not share common letters. If no such two words exist, return 0.
+ */
+const maxProduct = words => {
+  words.sort((a, b) => b.length - a.length);
+  let best = 0, bitsets = new Uint32Array(words.length);
+  for(let i = 0; i < words.length; i++) {
+    let word = words[i], wlen = word.length, bitset = 0;
+    if(wlen * words[0].length < best)
+      return best;
+    for(let j = 0; j < wlen; j++)
+      bitset |= 1 << (word.charCodeAt(j) - 97);
+    for(let j = 0; j < i; j++)
+      if((bitsets[j] & bitset) === 0)
+        best = Math.max(best, wlen * words[j].length);
+    bitsets[i] = bitset;
+  }
+  return best;
+};
