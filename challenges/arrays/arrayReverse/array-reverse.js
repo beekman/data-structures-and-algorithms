e337d86f45ee7f0cb14b6a66b36ba61ba2c8804e
Ben Beekman <bbeekman@gmail.com>
2021-06-20 23:57:07 -0700

    started reorganization of challenges folder to group by type

--- challenges/arrays/arrayReverse/array-reverse.js
+++ challenges/arrays/arrayReverse/array-reverse.js
@@ -1,5 +1,5 @@
 function reverseArray(arr) {
-  for(let i = 0;i <= Math.floor((arr.length - 1) / 2);i++) {
+  for(let i = 0; i <= Math.floor((arr.length - 1) / 2); i++) {
     let el = arr[i];
     arr[i] = arr[arr.length - 1 - i];
     arr[arr.length - 1 - i] = el;
