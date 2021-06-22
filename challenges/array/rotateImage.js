40f93bb4d80336dfc31ff83118d3cff3e4296e53
Ben Beekman <bbeekman@gmail.com>
2021-06-21 18:28:27 -0700

    light housekeeping

---
+++ challenges/array/rotateImage.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+
+let rotate = matrix => {
+  let y = matrix.length - 1;
+  for(let i = 0; i < matrix.length / 2; i++) {
+    let x = (matrix.length - 1) - i;
+    for(let j = i; j < y; j++) {
+      let temp = matrix[i][j];
+      matrix[i][j] = matrix[j][y];
+      matrix[j][y] = temp;
+      temp = matrix[i][j];
+      matrix[i][j] = matrix[y][x];
+      matrix[y][x] = temp;
+      temp = matrix[i][j];
+      matrix[i][j] = matrix[x][i];
+      matrix[x][i] = temp;
+      x--;
+    }
+    y--;
+  }
+  return matrix;
+};
