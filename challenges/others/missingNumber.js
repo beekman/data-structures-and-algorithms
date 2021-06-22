a41702f9d9abf328e4851a74ec016a15bd3de269
Ben Beekman <bbeekman@gmail.com>
2021-06-21 18:04:52 -0700

    solution for missingNumber challenge

---
+++ challenges/others/missingNumber.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ * Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ */
+const missingNumber = nums => {
+  const sum = nums.reduce((acc, val) => acc + val);
+  const count = nums.length;
+  const fullSum = (count * (count + 1)) / 2;
+  return (fullSum - sum);
+};
