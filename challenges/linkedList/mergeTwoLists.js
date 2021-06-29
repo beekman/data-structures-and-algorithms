b421efac05526e9575d95330e98abdadceecb393
Ben Beekman <bbeekman@gmail.com>
2021-06-28 17:11:01 -0700

    solution for mergeTwoLists

---
+++ challenges/linkedList/mergeTwoLists.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const mergeTwoLists = (l1, l2) => {
+  let mergedHead = { val: null, next: null };
+  let curr = mergedHead;
+
+  while(l1 && l2) {
+    if(l1.val < l2.val) {
+      curr.next = l1;
+      l1 = l1.next;
+    } else {
+      curr.next = l2;
+      l2 = l2.next;
+    }
+    curr = curr.next;
+  }
+  curr.next = l1 || l2;
+  return mergedHead.next;
+};
