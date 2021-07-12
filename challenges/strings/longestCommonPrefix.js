8b48433b2ecb13f03e9f0c774ad1f3ca2ccb1c0d
Ben Beekman <bbeekman@gmail.com>
2021-07-11 23:57:46 -0700

    solution for Longest Common Prefix

---
+++ challenges/strings/longestCommonPrefix.js
@@ -0,0 +1,16 @@
+const longestCommonPrefix = strs => {
+  let prefix = "";
+  if(strs === null || strs.length === 0) return prefix;
+
+  for(let i = 0; i < strs[0].length; i++) {
+    const char = strs[0][i]; // loop through all characters of the very first string.
+
+    for(let j = 1; j < strs.length; j++) {
+      // loop through all other strings in the array
+      if(strs[j][i] !== char) return prefix;
+    }
+    prefix = prefix + char;
+  }
+
+  return prefix;
+};
