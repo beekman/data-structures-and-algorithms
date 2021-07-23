1cbd9b1f56e7f9ef838394b656d7070f9b839620
Ben Beekman <bbeekman@gmail.com>
2021-07-23 00:33:54 -0700

    improved solution for Reverse Bits

--- challenges/others/reverseBits.js
+++ challenges/others/reverseBits.js
@@ -33,11 +33,12 @@ The input must be a binary string of length 32 */
  * @param {number} n - a positive integer
  * @return {number} - a positive integer
  */
- var reverseBits = n=>  {
-   let res = 0;
-   for (let i = 0; i < 32; i++) {
-      res = (res << 1) + (n & 1);
-      n = n >> 1;
-   }
-   return res;
- }
+const reverseBits = n => {
+  let res = 0;
+  for(let i = 0; i < 32; i++) {
+    res = (res << 1) + (n & 1);
+    n = n >>> 1;
+  }
+
+  return res >>> 0;
+};
