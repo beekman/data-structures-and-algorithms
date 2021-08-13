c437d40c5479783df12b18b2cc3c64ad7d1ec4b9
Ben Beekman <bbeekman@gmail.com>
2021-08-13 14:57:40 -0700

    added line-by-line comments

--- challenges/array/groupAnagrams.js
+++ challenges/array/groupAnagrams.js
@@ -13,11 +13,18 @@ Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
 const groupAnagrams = (strs) => {
   const groups = {};
   for(let i = 0; i < strs.length; i++) {
+    // sort the string so identical sets of letters all match the same key
     let sort = strs[i].split('').sort().join('');
+    // if the sorted string doesn't exist, add it as a key to the hash table
     if(!groups[sort]) {
+      //  set the key's value to an array containing the unsorted string
       groups[sort] = [strs[i]];
     }
-    else groups[sort].push(strs[i]);
+    else {
+      //if the sorted string already exists as a key, push the unsorted string into the existing array of values for that key.
+      groups[sort].push(strs[i]);
+    }
   }
+  // return the hash table values for each key as an array of arrays
   return Object.values(groups);
 };
