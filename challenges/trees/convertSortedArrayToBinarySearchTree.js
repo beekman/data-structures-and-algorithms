b8c3beca8c2e30f8b276b7a33eee936d6e84d48a
Ben Beekman <bbeekman@gmail.com>
2021-08-13 21:15:32 -0700

    solution for Convert Sorted Array To Binary Search Tree

---
+++ challenges/trees/convertSortedArrayToBinarySearchTree.js
@@ -0,0 +1,33 @@
+/*
+Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+A height-balanced binary tree is a binary tree in which the depth of the two subtrees of every node never differs by more than one.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number[]} nums
+ * @return {TreeNode}
+ */
+
+const sortedArrayToBST = nums => {
+  if(!nums.length) return null;
+
+  //Set the center num in each subarray as the root node
+  const mid = Math.floor(nums.length / 2);
+  const root = new TreeNode(nums[mid]);
+
+  // set all nums to the left and right as sub trees
+  // call the function recursively on each subtree
+  root.left = sortedArrayToBST(nums.slice(0, mid));
+  root.right = sortedArrayToBST(nums.slice(mid + 1));
+
+  return root;
+};
