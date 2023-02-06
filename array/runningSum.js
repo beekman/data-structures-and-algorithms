023c999eb27cc1bb17b93adee8199d778afa5ce0
Ben Beekman <bbeekman@gmail.com>
2023-02-06 12:52:29 -0800

    faster solution for runningSum

--- array/runningSum.js
+++ array/runningSum.js
@@ -1,11 +1,36 @@
-const runningSum = (nums) => {
-  let sum = 0;
-  const totalCounts = [];
-  nums.map(number => {
-    sum += number;
-    totalCounts.push(sum);
-  });
-  return totalCounts;
+/*
+Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
+
+Return the running sum of nums.
+
+Example 1:
+
+Input: nums = [1,2,3,4]
+Output: [1,3,6,10]
+Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+Example 2:
+
+Input: nums = [1,1,1,1,1]
+Output: [1,2,3,4,5]
+Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
+Example 3:
+
+Input: nums = [3,1,2,10,1]
+Output: [3,4,6,16,17]
+*/
+
+/**
+* @param {number[]} nums
+* @return {number[]}
+*/
+const runningSum = nums => {
+	for(let i = 0; i < nums.length - 1; i++) {
+		//insert nums[i]+nums[i+1] into temp variable
+		let temp = nums[i] + nums[i + 1];
+		//insert temp into num[i+1]
+		nums[i + 1] = temp;
+	}
+	return nums;~
 };
 
 module.exports = { runningSum };
