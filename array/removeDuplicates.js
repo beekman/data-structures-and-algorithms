3b77af46251a150ee3dd0db70e3e2cf0634802b9
Ben Beekman <bbeekman@gmail.com>
2023-02-07 12:53:37 -0800

    faster implementation of removeDuplicates

--- array/removeDuplicates.js
+++ array/removeDuplicates.js
@@ -1,15 +1,27 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same.
+
+Since it is impossible to change the length of the array in some languages, you must instead have the result be placed in the first part of the array nums. More formally, if there are k elements after removing the duplicates, then the first k elements of nums should hold the final result. It does not matter what you leave beyond the first k elements.
+
+Return k after placing the final result in the first k slots of nums.
+
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
+*/
+
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
-let removeDuplicates = (nums) => {
-  for(let i = 1; i < nums.length; i++) {
-    if(nums[i] === nums[i - 1]) {
-      nums.splice(i, 1);
-      i--;
+const removeDuplicates = nums => {
+    let i=0;
+    for (j=0; j<nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            i++;
+            nums[i] = nums[j];
+        }
     }
-  }
-  return nums.length;
+    return i + 1
 };
 
 export default removeDuplicates;
