9482c713fbdb7aef0cb90d00669f77d98f3bfb12
Ben Beekman <bbeekman@gmail.com>
2023-06-12 19:57:59 -0700

    solution for equalPairs

---
+++ hashMap/equalPairs.js
@@ -0,0 +1,53 @@
+/*
+Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj) such that row ri and column cj are equal.
+
+A row and column pair is considered equal if they contain the same elements in the same order (i.e., an equal array).
+
+Example 1:
+Input: grid = [[3,2,1],[1,7,6],[2,7,7]]
+Output: 1
+Explanation: There is 1 equal row and column pair:
+- (Row 2, Column 1): [2,7,7]
+
+Example 2:
+Input: grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]
+Output: 3
+Explanation: There are 3 equal row and column pairs:
+- (Row 0, Column 0): [3,1,2,2]
+- (Row 2, Column 2): [2,4,2,2]
+- (Row 3, Column 2): [2,4,2,2]
+ */
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+const equalPairs = grid => {
+    const columns = new Map();
+    const rows = new Map();
+    let rowNums, colNums, rowHash, colHash;
+    let ans = 0;
+
+    for (let i = 0; i < grid.length; i++) {
+        rowNums = [];
+        colNums = [];
+
+        for (let j = 0; j < grid[i].length; j++) {
+            rowNums.push(grid[i][j]);
+            colNums.push(grid[j][i]);
+        }
+
+        rowHash = rowNums.join(',');
+        colHash = colNums.join(',');
+
+        rows.set(rowHash, (rows.get(rowHash) || 0) + 1);
+        columns.set(colHash, (columns.get(colHash) || 0) + 1);
+    }
+
+    for (const [row, value] of rows) {
+        if (columns.has(row)) {
+            ans += value * columns.get(row);
+        }
+    }
+
+    return ans;
+};
