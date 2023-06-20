69b1d213b28b643c2e2d224392dfc7e555585ee2
Ben Beekman <bbeekman@gmail.com>
2023-06-20 16:36:28 -0700

    added comments

--- linkedList/intersectionOfTwoLinkedLists.js
+++ linkedList/intersectionOfTwoLinkedLists.js
@@ -19,8 +19,10 @@ Note that the linked lists must retain their original structure after the functi
 const getIntersectionNode = (headA, headB) => {
     let a = headA, b = headB;
     while (a !== b) {
-        a = !a ? headB : a.next;
-        b = !b ? headA : b.next;
+        // line up the ends of the lists by switching to the other list's head when we reach the end
+        a = !a ? headB : a.next; // if a is null, set a to headB, else set a to a.next
+        b = !b ? headA : b.next; // if b is null, set b to headA, else set b to b.next
     }
+    // a and b will either be null or the intersection node
     return a;
 };
