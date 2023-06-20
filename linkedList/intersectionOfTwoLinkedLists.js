090cc967e39af46a7fc08e7360d765ed107f1082
Ben Beekman <bbeekman@gmail.com>
2023-06-20 13:01:14 -0700

    description for intersectionOfTwoLinkedLists

--- linkedList/intersectionOfTwoLinkedLists.js
+++ linkedList/intersectionOfTwoLinkedLists.js
@@ -1,3 +1,8 @@
+/*
+Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+The test cases are generated such that there are no cycles anywhere in the entire linked structure.
+Note that the linked lists must retain their original structure after the function returns.
+*/
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
