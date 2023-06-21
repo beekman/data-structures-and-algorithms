eacf06a4d5ec0ca27d8ae48e3924c38b949e1f52
Ben Beekman <bbeekman@gmail.com>
2023-06-21 16:09:49 -0700

    refactoring

--- array/minimumCostToMakeArrayEqual.js
+++ array/minimumCostToMakeArrayEqual.js
@@ -25,10 +25,14 @@ const minCost = (nums, cost) => {
     const n = nums.length;
     if (n <= 1) return 0;
     let arr = [];
-    for (let i = 0; i < n; i++) arr[i] = [nums[i], cost[i]];
+    for (let i = 0; i < n; i++) {
+        arr[i] = [nums[i], cost[i]];
+    }
     arr.sort((a, b) => a[0] - b[0]);
     let psum = [arr[0][1]];
-    for (let i = 1; i < n; i++) psum.push(psum[i - 1] + arr[i][1]);
+    for (let i = 1; i < n; i++) {
+        psum.push(psum[i - 1] + arr[i][1]);
+    }
     let sum = 0;
     for (let i = 1; i < n; i++) {
         sum += (Math.abs(arr[i][0] - arr[0][0])) * arr[i][1];
